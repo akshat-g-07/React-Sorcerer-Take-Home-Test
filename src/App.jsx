a13f7c966e0d70206d717c4481ee575d63590184
akshat-g-07 <akshatg805@gmail.com>
2024-01-15 22:56:35 +0530

    Heading Feature
    Integrated Heading Feature
    Changes to tailwind config to deal with h1 tag
    Changes to index to deal with global error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,92 @@
 import "./App.css";
+import { useState } from "react";
+import { Editor, EditorState, RichUtils, Modifier } from "draft-js";
+import "../node_modules/draft-js/dist/Draft.css";
 
 function App() {
-  return <></>;
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const handleBeforeInput = (chars, editorState) => {
+    const selection = editorState.getSelection();
+    const contentState = editorState.getCurrentContent();
+    const currentBlock = contentState.getBlockForKey(selection.getStartKey());
+
+    if (
+      selection.getStartOffset() === 1 &&
+      chars === " " &&
+      currentBlock.getText()[0] === "#"
+    ) {
+      const newContentState = Modifier.replaceText(
+        contentState,
+        selection.merge({
+          anchorOffset: 0,
+          focusOffset: 1,
+        }),
+        ""
+      );
+
+      const newEditorState = EditorState.set(editorState, {
+        currentContent: newContentState,
+      });
+      const finalEditorState = RichUtils.toggleBlockType(
+        newEditorState,
+        "header-one"
+      );
+
+      const updatedEditorState = EditorState.moveFocusToEnd(finalEditorState);
+
+      setEditorState(updatedEditorState);
+      return true;
+    }
+  };
+
+  const handleKeyCommand = (command, editorState) => {
+    if (
+      command === "split-block" &&
+      RichUtils.getCurrentBlockType(editorState) !== "unstyled"
+    ) {
+      const contentState = editorState.getCurrentContent();
+      const selectionState = editorState.getSelection();
+      const splitContentState = Modifier.splitBlock(
+        contentState,
+        selectionState
+      );
+      const splitEditorState = EditorState.push(
+        editorState,
+        splitContentState,
+        "split-block"
+      );
+
+      const finalEditorState = RichUtils.toggleBlockType(
+        splitEditorState,
+        "unstyled"
+      );
+
+      setEditorState(finalEditorState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+
+  const handleReturn = (e, editorState) => {
+    // console.log(e);
+  };
+
+  return (
+    <>
+      <div className="bg-red-500 h-screen w-screen">
+        <Editor
+          editorState={editorState}
+          onChange={setEditorState}
+          handleBeforeInput={handleBeforeInput}
+          handleReturn={handleReturn}
+          handleKeyCommand={handleKeyCommand}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
